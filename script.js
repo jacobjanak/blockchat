d4bc2b694d31cefc0bb22326442f015600825bca
Jacob Janak <jakeathotmail@gmail.com>
2022-03-14 15:26:31 -0400

    Adding in my own smart contract

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ let account = null;
 const hasEthereum = typeof window.ethereum !== 'undefined';
 const hasMetamask = hasEthereum ? ethereum.isMetaMask : false;
 if (!hasEthereum || !hasMetamask) {
-  	window.alert('Error: Metamask not detected');
+  	window.alert('Error: Metamask not detected. Please install Metamask.');
+  	// NOTE: create a function to run the page without metamask
 }
 
 // Button to prompt user to enable metamask.
@@ -13,11 +14,12 @@ document.getElementById('enableMetamask').onclick = function() {
 }
 
 async function enableMetamask() {
-	const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+	const accounts = await ethereum.request({
+		method: 'eth_requestAccounts'
+	});
 	account = accounts[0];
 	console.log(account);
 }
-
 
 document.getElementById('sendTransaction').onclick = function() {
 	sendTransaction();
@@ -28,10 +30,10 @@ async function sendTransaction() {
 	  	nonce: '0x00', // ignored by MetaMask
 	  	gasPrice: '0x09184e72a000', // customizable by user during MetaMask confirmation.
 	  	gas: '0x2710', // customizable by user during MetaMask confirmation.
-	  	to: '0x0000000000000000000000000000000000000000', // Required except during contract publications.
+	  	to: '0xacb241f59e1a8c7a61f0781aed7ad067269feb26', // Required except during contract publications.
 	  	from: account, // must match user's active address.
 	  	value: '0x00', // Only required to send ether to the recipient from the initiating external account.
-	  	data: '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
+	  	data: '0xfcc74f71aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaccbbb', // Optional, but used for defining smart contract creation and interaction.
 	  	chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
 	};
 
