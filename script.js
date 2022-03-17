139a674163f33ac00d48f84bc1c2fa0ffc099a6e
Jacob Janak <jakeathotmail@gmail.com>
2022-03-16 23:06:10 -0400

    Everything works nicely now. Just need to prettify everything and make it better.

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 console.log('Update: 16'); 
 
 // DOM references.
-const contractTextDisplay = document.getElementById('contract-text-display');
-const enableMetamaskButton = document.getElementById('enable-metamask');
-const contractForm = document.getElementById('contract-text-update-form');
-const contractTextInput = document.getElementById('contract-text-input');
+const $contractTextDisplay = $('#contract-text-display');
+const $enableMetamaskButton = $('#enable-metamask-button');
+const $contractForm = $('#contract-text-update-form');
+const $contractTextInput = $('#contract-text-input');
 
 // User Ethereum wallet public address.
 let walletAddress = null;
@@ -45,21 +45,16 @@ const contract = {
 	}
 }
 
-// Check if user has MetaMask installed.
-function hasMetamask () {
-	return window.ethereum ? ethereum.isMetaMask : false;
-}
-
 // Read data from the smart contract and display it.
 if (hasMetamask()) {
 	contract.read(function(hex) {
 		contract.storage.text = hex;
-		contractTextDisplay.textContent = hexToString(contract.storage.text);
+		$contractTextDisplay.text(hexToString(contract.storage.text));
 	});
 }
 
 // Button to prompt user to enable metamask.
-enableMetamaskButton.onclick = function() { 
+$enableMetamaskButton.on('click', function() { 
 	// NOTE: Should I check for MetaMask or is ethereum enough?
 	if (!hasMetamask()) {
 	  	window.alert("Error: MetaMask not detected. Please install MetaMask.");
@@ -71,21 +66,12 @@ enableMetamaskButton.onclick = function() {
 		// chainId: string; // A 0x-prefixed hexadecimal string)
 		// chainId: 42, // kovan chain id (maybe 0x42 or 0x2a ?)
 	}
-}
-
-// Prompt user to enable MetaMask through the extension.
-async function enableMetamask() {
-	const accounts = await ethereum.request({
-		method: 'eth_requestAccounts'
-	});
-	walletAddress = accounts[0];
-	console.log(walletAddress);
-}
+})
 
 // Form to update the text in the smart contract.
-contractForm.onsubmit = function(event) {
+$contractForm.on('submit', function(event) {
 	event.preventDefault();
-	const input = contractTextInput.value.trim();
+	const input = $contractTextInput.val().trim();
 
 	// Input must be converted to a hex number of length 64.
 	let hex = stringToHex(input);
@@ -96,7 +82,21 @@ contractForm.onsubmit = function(event) {
 	contract.write(hex, function(txHash) {
 		console.log(txHash);
 	})
-};
+});
+
+// Prompt user to enable MetaMask through the extension.
+async function enableMetamask() {
+	const accounts = await ethereum.request({
+		method: 'eth_requestAccounts'
+	});
+	walletAddress = accounts[0];
+	console.log(walletAddress);
+}
+
+// Check if user has MetaMask installed.
+function hasMetamask () {
+	return window.ethereum ? ethereum.isMetaMask : false;
+}
 
 // Convert hexadecimal number to string using char codes.
 // NOTE: Ignores '0x'.
