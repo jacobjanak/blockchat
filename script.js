8e134ec0a162afc36e866996a750ce4ac4311d85
Jacob Janak <jakeathotmail@gmail.com>
2022-03-17 16:31:49 -0400

    Added tooltip to wallet display.

--- script.js
+++ script.js
@@ -1,134 +1,143 @@
-// Update counter for GitHub pages.
-console.log('Update: 18'); 
+$(document).ready(function() {
 
-// User Ethereum wallet public address.
-let walletAddress = '';
+	// Update counter for GitHub pages.
+	console.log('Update: 18');
 
-// Smart contract.
-const contract = {
-	address: '0xacB241f59E1a8c7A61f0781aed7Ad067269feb26', // Contract public address
-	writeAddress: '0xfcc74f71', // Hex address of the write method of the contract.
-	storage: { text: null }, // Contract storage contains only bytes32 public text.
+	// Enable all tooltips.
+	$('[data-toggle="tooltip"]').tooltip();
 
-	// Read data from the contract.
-	read: async function(callback) {
-		const text = await ethereum.request({
-		  	method: 'eth_getStorageAt',
-		  	params: [ this.address, '0x0' ],
-		})
-		if (callback) callback(text);
-	},
+	// User Ethereum wallet public address.
+	let walletAddress = '';
 
-	// Update the storage of the smart contract.
-	write: async function(data, callback) {
-		const txHash = await ethereum.request({
-		  	method: 'eth_sendTransaction',
-		  	params: [{
-			  	to: this.address,
-			  	from: walletAddress,
-			  	data: this.writeAddress + (data || ''),
-			}],
+	// Smart contract.
+	const contract = {
+		address: '0xacB241f59E1a8c7A61f0781aed7Ad067269feb26', // Contract public address
+		writeAddress: '0xfcc74f71', // Hex address of the write method of the contract.
+		storage: { text: null }, // Contract storage contains only bytes32 public text.
+
+		// Read data from the contract.
+		read: async function(callback) {
+			const text = await ethereum.request({
+			  	method: 'eth_getStorageAt',
+			  	params: [ this.address, '0x0' ],
+			})
+			if (callback) callback(text);
+		},
+
+		// Update the storage of the smart contract.
+		write: async function(data, callback) {
+			const txHash = await ethereum.request({
+			  	method: 'eth_sendTransaction',
+			  	params: [{
+				  	to: this.address,
+				  	from: walletAddress,
+				  	data: this.writeAddress + (data || ''),
+				}],
+			});
+			if (callback) callback(txHash);
+		}
+	}
+
+	// Read data from the smart contract and display it.
+	if (hasMetamask()) {
+		contract.read(function(hex) {
+			contract.storage.text = hex;
+			const str = hexToString(contract.storage.text);
+
+			// Dynamically set font size according to text length.
+			// let fontSize = Math.ceil(100 / str.length);
+			// if (fontSize > 20) fontSize = 20;
+			// else if (fontSize < 2) fontSize = 2; // Should never happen.
+			// fontSize = fontSize + 'vw';
+
+			// Update DOM.
+			$('#contract-text-display').text(str); // .css({ fontSize });
 		});
-		if (callback) callback(txHash);
-	}
-}
-
-// Read data from the smart contract and display it.
-if (hasMetamask()) {
-	contract.read(function(hex) {
-		contract.storage.text = hex;
-		const str = hexToString(contract.storage.text);
-
-		// Dynamically set font size according to text length.
-		// let fontSize = Math.ceil(100 / str.length);
-		// if (fontSize > 20) fontSize = 20;
-		// else if (fontSize < 2) fontSize = 2; // Should never happen.
-		// fontSize = fontSize + 'vw';
-
-		// Update DOM.
-		$('#contract-text-display').text(str); // .css({ fontSize });
-	});
-}
-
-// Button to prompt user to enable metamask.
-$('.connect-wallet').on('click', function() { 
-	// NOTE: Should I check for MetaMask or is ethereum enough?
-	if (!hasMetamask()) {
-	  	window.alert("Error: MetaMask not detected. Please install MetaMask.");
-	  	// TO DO: Create a function to run the page without metamask.
 	} else {
-		enableMetamask();
-		// NOTE: Switch user to Rovan network.
-		// wallet_switchEthereumChain
-		// chainId: string; // A 0x-prefixed hexadecimal string)
-		// chainId: 42, // kovan chain id (maybe 0x42 or 0x2a ?)
+		$('#contract-text-display').text("No Ethereum Wallet Detected");
 	}
-})
 
-// Button to show/hide the text update form.
-$('#change-text').on('click', function() {
-	$('#form-container').toggle();
-})
-
-// Form to update the text in the smart contract.
-$('#contract-text-update-form').on('submit', function(e) {
-	e.preventDefault();
-	const input = $('#contract-text-input').val().trim();
-
-	// Input must be converted to a hex number of length 64.
-	let hex = stringToHex(input);
-	while (hex.length < 64) hex += '0';
-	console.log(hex)
-
-	// Send to blockchain.
-	contract.write(hex, function(txHash) {
-		console.log(txHash);
-		// TO DO: Give user some feedback.
+	// Button to prompt user to enable metamask.
+	$('.connect-wallet').on('click', function() { 
+		// NOTE: Should I check for MetaMask or is ethereum enough?
+		if (!hasMetamask()) {
+		  	window.alert("Error: MetaMask not detected. Please install MetaMask.");
+		  	// TO DO: Create a function to run the page without metamask.
+		} else {
+			enableMetamask();
+			// NOTE: Switch user to Rovan network.
+			// wallet_switchEthereumChain
+			// chainId: string; // A 0x-prefixed hexadecimal string)
+			// chainId: 42, // kovan chain id (maybe 0x42 or 0x2a ?)
+		}
 	})
-});
 
-$('#contract-text-input').on('input', function() {
-	const input = $('#contract-text-input').val();
-	$('#char-count').text(input.length);
-})
+	// Button to show/hide the text update form.
+	$('#change-text').on('click', function() {
+		$('#form-container').toggle();
+	})
 
-// Check if user has MetaMask installed.
-function hasMetamask () {
-	return window.ethereum ? ethereum.isMetaMask : false;
-}
+	// Form to update the text in the smart contract.
+	$('#contract-text-update-form').on('submit', function(e) {
+		e.preventDefault();
+		const input = $('#contract-text-input').val().trim();
 
-// Prompt user to enable MetaMask through the extension.
-async function enableMetamask() {
-	const accounts = await ethereum.request({
-		method: 'eth_requestAccounts'
+		// Input must be converted to a hex number of length 64.
+		let hex = stringToHex(input);
+		while (hex.length < 64) hex += '0';
+		console.log(hex)
+
+		// Send to blockchain.
+		contract.write(hex, function(txHash) {
+			console.log(txHash);
+			// TO DO: Give user some feedback.
+		})
 	});
-	walletAddress = accounts[0];
-	
-	// Update DOM.
-	$('.connect-wallet').hide();
-	$('#submit').show();
-	$('#wallet-address').text(walletAddress.slice(-4));
-	$('#wallet-display').show();
-}
 
-// Convert hexadecimal number to string using char codes.
-// NOTE: Ignores '0x'.
-function hexToString(hex) {
-    let str = '';
-    for (let i = 0; i < hex.length; i += 2) {
-        let v = parseInt(hex.substr(i, 2), 16);
-        if (v) str += String.fromCharCode(v);
-    }
-    return str;
-}
+	$('#contract-text-input').on('input', function() {
+		const input = $('#contract-text-input').val();
+		$('#char-count').text(input.length);
+	})
 
-// Convert string to hexadecimal number using char codes.
-// NOTE: Ignores weird characters, which is bad.
-function stringToHex(str) {
-	let hex = '';
-	for (let i = 0; i < str.length; i++) {
-		let v = str.charCodeAt(i).toString(16);
-		if (v.length == 2) hex += v;
+	// Check if user has MetaMask installed.
+	function hasMetamask () {
+		return window.ethereum ? ethereum.isMetaMask : false;
 	}
-	return hex;
-}
+
+	// Prompt user to enable MetaMask through the extension.
+	async function enableMetamask() {
+		const accounts = await ethereum.request({
+			method: 'eth_requestAccounts'
+		});
+		walletAddress = accounts[0];
+		
+		// Update DOM.
+		$('.connect-wallet').hide();
+		$('#submit').show();
+		$('#wallet-address').text(walletAddress.slice(-4));
+		$('#wallet-display').show();
+	}
+
+	// Convert hexadecimal number to string using char codes.
+	// NOTE: Ignores '0x'.
+	function hexToString(hex) {
+	    let str = '';
+	    for (let i = 0; i < hex.length; i += 2) {
+	        let v = parseInt(hex.substr(i, 2), 16);
+	        if (v) str += String.fromCharCode(v);
+	    }
+	    return str;
+	}
+
+	// Convert string to hexadecimal number using char codes.
+	// NOTE: Ignores weird characters, which is bad.
+	function stringToHex(str) {
+		let hex = '';
+		for (let i = 0; i < str.length; i++) {
+			let v = str.charCodeAt(i).toString(16);
+			if (v.length == 2) hex += v;
+		}
+		return hex;
+	}
+
+});
