c2dd97abfd0c049a2f65ea287af68c57f45787da
Jacob Janak <jakeathotmail@gmail.com>
2022-06-30 09:55:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -130,7 +130,9 @@ $(document).ready(function() {
 		// When this runs for the first time we want it to keep running.
 		if (repeat) {
 			setInterval(function() {
-				getContractData(false);
+				if ($('#contract-text-input').val()) {
+					getContractData(false);
+				}
 			}, 10 * 1000);
 		}
 	}
